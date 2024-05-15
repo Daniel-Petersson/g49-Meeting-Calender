0f96e3c3c994ab055ef487cdba3c73882b33e351
binleiden <daniel.petersson83@gmail.com>
2024-05-15 13:42:59 +0200

    refactored after MeetingCalendar class changed name

--- src/main/java/se/lexicon/dao/CalendarDao.java
+++ src/main/java/se/lexicon/dao/CalendarDao.java
@@ -1,14 +1,14 @@
 package se.lexicon.dao;
 
-import se.lexicon.model.Calendar;
+import se.lexicon.model.MeetingCalendar;
 
 import java.util.Collection;
 import java.util.Optional;
 
 public interface CalendarDao {
-    Calendar createCalender(String title, String username);
-    Optional<Calendar> findById(int id);
-    Collection<Calendar> findByUsername(String username);
-    Optional<Calendar> findByTitle(String title);
+    MeetingCalendar createCalender(String title, String username);
+    Optional<MeetingCalendar> findById(int id);
+    Collection<MeetingCalendar> findByUsername(String username);
+    Optional<MeetingCalendar> findByTitle(String title);
     boolean deleteCalender(int id);
 }
