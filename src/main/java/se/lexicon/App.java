1bf867aff74d27f602b7455dc6c915d86f7fa7ec
binleiden <daniel.petersson83@gmail.com>
2024-05-15 15:59:24 +0200

    Test of application

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -3,10 +3,13 @@ package se.lexicon;
 import com.mysql.cj.MysqlConnection;
 import se.lexicon.controller.CalendarController;
 import se.lexicon.dao.CalendarDao;
+import se.lexicon.dao.MeetingDao;
 import se.lexicon.dao.UserDao;
 import se.lexicon.dao.impl.CalendarDaoImpl;
+import se.lexicon.dao.impl.MeetingDaoImpl;
 import se.lexicon.dao.impl.UserDaoImpl;
 import se.lexicon.dao.impl.db.MeetingCalenderDBConnection;
+import se.lexicon.model.Meeting;
 import se.lexicon.view.CalendarConsoleUI;
 import se.lexicon.view.CalendarView;
 
@@ -16,7 +19,7 @@ import java.sql.Connection;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
@@ -24,7 +27,9 @@ public class App
         CalendarView view = new CalendarConsoleUI();
         UserDao userdao = new UserDaoImpl(connection);
         CalendarDao calendarDao = new CalendarDaoImpl(connection);
-        CalendarController controller = new CalendarController(view,userdao,calendarDao);
+        MeetingDao meetingDao = new MeetingDaoImpl(connection);
+        CalendarController controller = new CalendarController(view,userdao,calendarDao,meetingDao);
         controller.run();
+
     }
 }
