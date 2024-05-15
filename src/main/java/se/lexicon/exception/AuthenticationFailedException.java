ef28ac628f082a2cbf216ea33c2116ce66ca2860
binleiden <daniel.petersson83@gmail.com>
2024-05-15 13:44:40 +0200

    added constructor

--- src/main/java/se/lexicon/exception/AuthenticationFailedException.java
+++ src/main/java/se/lexicon/exception/AuthenticationFailedException.java
@@ -4,4 +4,8 @@ public class AuthenticationFailedException extends Exception{
     public AuthenticationFailedException(String message, Throwable cause){
         super(message, cause);
     }
+
+    public AuthenticationFailedException(String message) {
+        super(message);
+    }
 }
